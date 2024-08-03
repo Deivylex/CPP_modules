526e031230b81765f47768328577cbecfd3e576c
deivy <deivylex93@gmail.com>
2024-08-03 13:21:15 +0300

    cpp01 ex06 done

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -1,5 +1,9 @@
 #include "Harl.hpp"
 
+Harl::Harl(){}
+
+Harl::~Harl(){}
+
 void Harl::debug( void )
 {
     std::cout << "[ DEBUG ]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger."<< std::endl;
