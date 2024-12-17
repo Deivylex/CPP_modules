8cd2c14dbd0eb340d6805a77cbc129099d85b0e0
Deivy Zurita <deivylex93@gmail.com>
2024-12-17 15:52:25 +0200

    starting cpp06

---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,18 @@
+#ifndef SCALARCONVERTER.HPP
+#define SCALARCONVERTER.HPP
+
+
+#include <iostream>
+
+class ScalarConverter
+{
+    private :
+
+    public :
+
+    static void convert(std::string number);
+
+};
+
+
+#endif
